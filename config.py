5e3e3818596630bf01ae5ff91a035e9d33f3874d
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-02-24 17:56:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,12 +32,12 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://clone:clone@cluster0.go11
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001736090857"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI'm a file share bot. I can store private files in Specified Channel and other users can access it from a special link.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
